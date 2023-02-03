8d1694c3fba1ef8da4208e24123f72a353c4c072
Getabefikadu <getabefikadu@gmail.com>
2023-02-03 10:31:00 +0300

    Some fix

--- src/main.ts
+++ src/main.ts
@@ -2,34 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app';
 import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { EntityNotFoundExceptionFilter } from './filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const builder = new DocumentBuilder()
-  // .setTitle('Perago Information systems')
-  // .setDescription('Organizational Hierarchy')
-  // .setVersion('1.0')
-  // .addTag('PIS')
-  // .build();
-
-  // const document = SwaggerModule.createDocument(app, builder);
-  // SwaggerModule.setup('api', app, document);
+  const builder = new DocumentBuilder()
+  .setTitle('Perago Information systems')
+  .setDescription('Organizational Hierarchy')
+  .setVersion('1.0')
+  .addTag('PIS')
+  .build();
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new EntityNotFoundExceptionFilter());
   app.enableCors();
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+
+  const document = SwaggerModule.createDocument(app, builder);
+  SwaggerModule.setup('api', app, document);
 
   const config = app.get(ConfigService)
   const port = config.get('APP_PORT') || 3333
   
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
 }
 
 bootstrap();
